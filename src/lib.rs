f6af523c92c1782311a764b5e8d3347becb4b75b
Kevin Laeufer <ekiwi@berkeley.edu>
2017-09-25 12:23:51 -0700

    test some signed/unsigned behavior

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
 #[test]
 fn it_works() {
 }
+
+#[test]
+fn sign_cast() {
+	assert_eq!((-1i8) as u8, 0xffu8);
+	assert_eq!((0xffu8) as i8, -1);
+}
+
+#[test]
+fn unsigned_subtraction() {
+	assert_eq!(8u8 - 5u8, 3u8);
+	// this panics
+	// assert_eq!(5u8 - 8u8, (-3i8) as u8);
+	// this does not
+	assert_eq!((5u8 as i8 - 8u8 as i8) as u8, (-3i8) as u8);
+}
